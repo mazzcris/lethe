e70039305869acbce20e063783d8e0037e4d63b4
mazzcris <cristiano.mazzarini@gmail.com>
2017-12-25 23:04:36 +0100

    Add bootstrap and buttons toolbar

--- App.jsx
+++ App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import ServiceButton from "./components/ServiceButton.jsx";
+import {ButtonToolbar} from 'react-bootstrap'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      'buttons': [
+        {'name': 'GitHub'},
+        {'name': 'Trello'},
+        {'name': 'Google Calendar'},
+      ]
+    }
   }
 
-  render() {
-    return <h1>Hello, World!</h1>;
+  render () {
+    let buttons = this.state.buttons.map((button, index) =>
+      <ServiceButton key={index} data={button}></ServiceButton>
+    );
+    return <div>
+      <ButtonToolbar className={"button-toolbar"}>
+        {buttons}
+      </ButtonToolbar>
+    </div>
   }
 }
 
